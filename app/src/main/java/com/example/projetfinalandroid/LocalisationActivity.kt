d9d362ecaa451d7fd44979eebe3dfc878dadb7cc
Hugo Madureira <hugoomdra@gmail.com>
2021-04-15 11:00:50 +0200

    ajout de la localisation

---
+++ app/src/main/java/com/example/projetfinalandroid/LocalisationActivity.kt
@@ -0,0 +1,121 @@
+package com.example.projetfinalandroid
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_localisation.*
+import java.util.*
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.CancellationTokenSource
+
+
+class LocalisationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_localisation)
+
+
+        btn_localisation.setOnClickListener({
+            requestPermission()
+        })
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            PERMISSION_REQUEST_LOCATION -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // Permission obtenue, Nous continuons la suite de la logique.
+                    getLocation()
+                } else {
+                    // TODO
+                    // Permission non accepté, expliqué ici via une activity ou une dialog pourquoi nous avons besoins de la permissions
+                }
+                return
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLocation() {
+        if (hasPermission()) {
+            val locationManager = applicationContext.getSystemService(LOCATION_SERVICE) as LocationManager?
+
+            locationManager?.run {
+                locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER)?.run {
+                    val distance = calculDistance(this)
+                    afficherDistance(distance)
+//                    geoCode(this)
+                }
+            }
+        }
+    }
+
+    private fun afficherDistance(distance : Double){
+//        Toast.makeText(this, distance.toString() + " km", Toast.LENGTH_SHORT).show()
+        text_distance.setText(distance.toString() + " km")
+    }
+
+    private fun calculDistance(location: Location): Double {
+
+        val p = Math.pow(10.0, 1.0) // Arrondir le nb de décimal
+        val destination = Location("eseo")
+        destination.latitude = 47.4937187
+        destination.longitude = -0.5504861
+
+        val distance = location.distanceTo(destination).toDouble()/1000 // Calcul de la distance en km
+
+        val finalDistance = Math.floor((distance) *p)/p // Distance arrondie
+
+        return finalDistance
+    }
+
+    private fun requestPermission() {
+
+        if (!hasPermission()) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), PERMISSION_REQUEST_LOCATION)
+        } else {
+            getLocation()
+        }
+    }
+
+    private fun hasPermission(): Boolean {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun geoCode(location: Location){
+        val geocoder = Geocoder(this, Locale.getDefault())
+        val results = geocoder.getFromLocation(location.latitude, location.longitude, 1)
+
+        if (results.isNotEmpty()) {
+            Toast.makeText(this, results[0].getAddressLine(0), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    companion object {
+
+        const val PERMISSION_REQUEST_LOCATION = 9999
+
+        fun getStartIntent(context: Context): Intent {
+            return Intent(context, LocalisationActivity::class.java)
+        }
+    }
+}
